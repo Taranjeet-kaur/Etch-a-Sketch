81447d0ce72d564114ccb4ef19ea92988c66338b
Taranjeet-kaur <137863748+Taranjeet-kaur@users.noreply.github.com>
2025-02-23 19:22:22 +0000

    Created a grid dynamically with js and added event listners

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 const container = document.querySelector(".container")
 
+let n = prompt("Enter a number!")
 
-function createDivs(n){
-    for (let index = 0; index < n; index++) {
-        let squareDivs = document.createElement("div")
-        squareDivs.style.height = "16px"
-        squareDivs.style.width = "16px"
-        squareDivs.style.border = "solid 1px black" 
-        squareDivs.classList.add("square")
-        container.appendChild(squareDivs)
+if(isNaN(n)||n<=0){
+    alert("Enter a valid positve number!")
+} else {
+    container.innerHTML= "";
+    container.style.gridTemplateColumns = `repeat(${n}, 1fr)`
+    container.style.gridTemplateRows= `repeat(${n}, 1fr)`
+    
+    function createDivs(){
+        for(let i=0; i<n; i++){
+            for(let j=0; j<n; j++){
+                let squareDiv = document.createElement("div")
+                squareDiv.classList.add("square")
+
+                squareDiv.addEventListener("mouseover", (event)=>{
+                 if((event.buttons === 1) ||(event.button === 2) ){
+                    
+                let r = Math.floor(Math.random() * 250 + 55)
+                let g = Math.floor(Math.random() * 250 + 55)
+                let b = Math.floor(Math.random() * 250 + 55)
+
+                squareDiv.style.backgroundColor = `rgb(${r},${g},${b})`}
+            })
+
+                container.appendChild(squareDiv)
+            }
+        }
     }
-}
 
-createDivs(10)
+    createDivs()
+}
